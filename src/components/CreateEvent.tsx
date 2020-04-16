5299547e1729ba7225c0ec3752337978a6cebf9f
MFAC <ninafroes@gmail.com>
2020-04-16 15:52:12 +0200

    feat: Create generic CreateEvent Component

---
+++ src/components/CreateEvent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CreateEvent() {
+  return (
+    <div>
+      <h1 style={{ color: "var(--main-color-orange, #f07422)" }}>Create Event</h1>
+      <h2 style={{ color: "var(--main-color-blue, #0c598a)"}}>This is the second heading</h2>
+      <h3 style={{ color: "var(--main-color-red, #bd0b2b)" }}>This is the third heading</h3>
+      <h4 style={{ color: "var(--main-color-grey, #eee)" }}>This is the fourth heading</h4>
+      <p>This is a paragraph</p>
+    </div>
+  )
+}
+
+export default CreateEvent
