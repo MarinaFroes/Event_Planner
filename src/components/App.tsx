6f0a41c3bf5b1bc8949485be4a9bbbb7d80ce448
MFAC <ninafroes@gmail.com>
2020-04-16 15:55:06 +0200

    feat: Delete boilerplate and import generic components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import '../App.css';
+import React from 'react'
+import '../styles/App.css'
+
+import CreateEvent from './CreateEvent'
+import NavBar from './NavBar'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <CreateEvent />
+      <Footer />
     </div>
   );
 }
