c1558dc735d28c3dea600facee56047b66c63203
MFAC <ninafroes@gmail.com>
2020-04-16 15:51:51 +0200

    feat: Create NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../assets/icons/noun_event_1004866.svg'
+
+function NavBar() {
+  return (
+    <div>
+      <img src={Logo} width="40px" alt=""/>
+      <p>NavBar</p>
+    </div>
+  )
+}
+
+export default NavBar;
