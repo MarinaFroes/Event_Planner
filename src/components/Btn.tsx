6ec750300c0c062740ced8e37df672108ea27e1d
MFAC <ninafroes@gmail.com>
2020-04-17 10:28:55 +0200

    styles: Change Btn height

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -5,7 +5,7 @@ const StyledButton = styled.button<{ primary: boolean }>`
   background: ${props => props.primary ? "var(--main-color-orange, #f07422)" : "var(--main-color-white, #fff)"};
   color: ${props => props.primary ? "var(--main-color-white, #fff)" : "var(--main-color-orange, #f07422)"};
   border: 1px solid var(--main-color-orange, #f07422);
-  height: 30px;
+  height: 40px;
   margin: 10px 0;
   font-size: 16px;
   border-radius: 5px;
