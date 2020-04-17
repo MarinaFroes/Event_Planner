58ee2ac33354ec243ee26d0a313e581386cc7967
MFAC <ninafroes@gmail.com>
2020-04-17 15:15:08 +0200

    feat: Update NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,65 @@
 import React from 'react'
-import Logo from '../assets/icons/noun_event_1004866.svg'
+import styled from 'styled-components'
+import { FaBars } from 'react-icons/fa'
+
+import Logo from '../assets/icons/logo.svg'
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 40px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+`
+
+const StyledLogo = styled.img`
+  height: 30px;
+  margin-left: 10px;
+  padding-right: 5px;
+`
+
+const Title = styled.p`
+  font-size: 16px;
+  color: var(--main-color-orange, #f07422);
+  margin: auto;
+
+  @media only screen and (max-width: 500px){
+    display: none;
+  }
+`
+
+const LogoLink = styled.a`
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+
+  :hover {
+    cursor: pointer;
+  }
+`
+
+const Hamburger = styled.button`
+  display: flex;
+  align-items: center;
+  padding-right: 10px;
+  font-size: 24px;
+  border: none;
+  background-color: transparent;
+`
 
 const NavBar: React.FC = () => {
   return (
-    <div>
-      <img src={Logo} width="40px" alt=""/>
-      <p>NavBar</p>
-    </div>
+    <StyledNav>
+      <LogoLink href="/">
+        <StyledLogo src={Logo} alt="" />
+        <Title>Event Planner</Title>
+      </LogoLink>
+      <Hamburger>
+        <FaBars/>
+      </Hamburger>
+    </StyledNav>
   )
 }
 
