b6ab40fc700cdfdaf5e168b94f1f5f074083a4ff
MFAC <ninafroes@gmail.com>
2020-04-18 10:48:00 +0200

    feat: Add Footer generic content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 200px;
+  background-color: var(--main-color-white, #fff);
+  background-color: #eee;
+`
 
 const Footer: React.FC = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <StyledFooter>
+      <p>Footer Info</p>
+    </StyledFooter>
   )
 }
 
