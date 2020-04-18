ecd433c9e3006c44f366aa20e3a1b582120d8913
MFAC <ninafroes@gmail.com>
2020-04-18 10:46:54 +0200

    feat: Import TextBox component and remove secondary button

--- src/components/CreateEvent.tsx
+++ src/components/CreateEvent.tsx
@@ -4,22 +4,27 @@ import Btn from './Btn'
 import Header from './Header'
 import EventForm from './EventForm'
 import MainImg from '../assets/images/spencer-davis-vJsj-hgOEG0-unsplash.jpg'
+import TextBox from './TextBox'
 
 const CreateEvent: React.FC = () => {
   const subtitle = "Event planner is an easy way to create events and invite friends, sharing the costs and the tasks."
+  const heading1 = "Add your event info"
   return (
     <div>
-      <Header title="Event Planner" subtitle={subtitle} imageUrl={MainImg}/>
-      <EventForm handleChange={e => console.log(e.target.value)}/>
-      <Btn primary={true} text="Create Event" widthSize="300px"/>
-      <Btn primary={false} text="Edit Event" widthSize="300px"/>
-      <h1 style={{ color: "var(--main-color-orange, #f07422)" }}>Create Event</h1>
-      <h2 style={{ color: "var(--main-color-blue, #0c598a)"}}>This is the second heading</h2>
-      <h3 style={{ color: "var(--main-color-red, #bd0b2b)" }}>This is the third heading</h3>
-      <h4 style={{ color: "var(--main-color-grey, #eee)" }}>This is the fourth heading</h4>
-      <p>This is a paragraph</p>
+      <Header title="Event Planner" subtitle={subtitle} imageUrl={MainImg} />
+      <TextBox heading1={heading1} heading2="" />
+      <EventForm showImage={true} handleChange={e => console.log(e.target.value)}/>
+      <Btn primary={true} text="Create Event" widthSize="300px"/>      
     </div>
   )
 }
 
 export default CreateEvent
+
+  /**
+   * <h1 style={{ color: "var(--main-color-orange, #f07422)" }}>Create Event</h1>
+      <h2 style={{ color: "var(--main-color-blue, #0c598a)"}}>This is the second heading</h2>
+      <h3 style={{ color: "var(--main-color-red, #bd0b2b)" }}>This is the third heading</h3>
+      <h4 style={{ color: "var(--main-color-grey, #eee)" }}>This is the fourth heading</h4>
+      <p>This is a paragraph</p>
+   */
