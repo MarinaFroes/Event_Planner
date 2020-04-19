64664f10c57467303613bb6374ab6121cc21793f
MFAC <ninafroes@gmail.com>
2020-04-19 18:20:44 +0200

    styles: Add media queries

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -5,6 +5,14 @@ const Heading1 = styled.h1`
   font-size: 26px;
   margin: 20px;
   color: var(--main-color-blue, #0c598a);
+
+  @media only screen and (min-width: 768px){
+    font-size: 26px;
+  }
+
+  @media only screen and (min-width: 1080px){
+    font-size: 42px;
+  }
 `
 
 const Heading2 = styled.h2`
@@ -12,6 +20,14 @@ const Heading2 = styled.h2`
   font-weight: normal;
   color: var(--main-color-black, #000);
   margin: 20px;
+
+  @media only screen and (min-width: 768px){
+    font-size: 20px;
+  }
+
+  @media only screen and (min-width: 1080px){
+    font-size: 26px;
+  }
 `
 
 interface Props {
