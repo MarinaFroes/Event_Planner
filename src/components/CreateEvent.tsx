a91cb4960ff3e58cb19311c158b9babcac00fbc5
MFAC <ninafroes@gmail.com>
2020-04-20 08:47:31 +0200

    feat: Import MainHeader instead of Header

--- src/components/CreateEvent.tsx
+++ src/components/CreateEvent.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Header from './Header'
+import MainHeader from './MainHeader'
 import EventForm from './EventForm'
 import MainImg from '../assets/images/spencer-davis-vJsj-hgOEG0-unsplash.jpg'
 
@@ -10,12 +10,17 @@ const CreateEventContainer = styled.div`
 `
 
 const CreateEvent: React.FC = () => {
+  const title = "Event Planner"
   const subtitle = "Event planner is an easy way to create events and invite friends, sharing the costs and the tasks."
   const heading1 = "Add your event info"
 
   return (
     <CreateEventContainer>
-      <Header title="Event Planner" subtitle={subtitle} imageUrl={MainImg}/>
+      <MainHeader
+        title={title}
+        subtitle={subtitle}
+        imageUrl={MainImg}
+      />
       
       <EventForm
         heading1={heading1}
@@ -31,11 +36,3 @@ const CreateEvent: React.FC = () => {
 }
 
 export default CreateEvent
-
-  /**
-   * <h1 style={{ color: "var(--main-color-orange, #f07422)" }}>Create Event</h1>
-      <h2 style={{ color: "var(--main-color-blue, #0c598a)"}}>This is the second heading</h2>
-      <h3 style={{ color: "var(--main-color-red, #bd0b2b)" }}>This is the third heading</h3>
-      <h4 style={{ color: "var(--main-color-grey, #eee)" }}>This is the fourth heading</h4>
-      <p>This is a paragraph</p>
-   */
