d51db974cf3dcba50c5062fb21396ec4ae772e9e
MFAC <ninafroes@gmail.com>
2020-04-21 09:53:03 +0200

    feat: Add EventCard

--- src/components/AcceptInvite.tsx
+++ src/components/AcceptInvite.tsx
@@ -5,22 +5,37 @@ import Header from './Header'
 import EventImg from '../assets/images/tuva-mathilde-loland-4rfVL3NNGrA-unsplash.jpg'
 import TextBox from './TextBox'
 import Btn from './Btn'
+import EventCard from './EventCard'
 
 const AcceptInviteContainer = styled.div`
   background-color: var(--main-color-grey, #eee);
   display: flex;
   flex-direction: column;
   align-items: center;
+
 `
 
 const Content = styled.div`
+  display: flex;
+  flex-direction: column;
   padding: 20px;
+
+
+  @media only screen and (min-width: 1024px){
+    flex-direction: row;
+  }
+`
+
+const TasksContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
 `
 
 const AcceptInvite: React.FC = () => {
-  const eventName = "My Birthday Details"
+  const eventName = "My Birthday"
   const subtitle = "You were invited to take part on My Birthday. Check the event info below:"
-  const heading1 = "Accept the invite by signing up"
+  const heading1 = "Sign up to accept the invite"
   const heading2 = "After accepting the invite, the host will be notified. If the host confirms your participation on the event or updates any event info, you will receive a notification by email."
   const eventText = {
     description: "Chocolate cake",
@@ -31,6 +46,32 @@ const AcceptInvite: React.FC = () => {
     participants: 5,
     cost: 20.00
   }
+  const tasks = [
+    {
+      id: "task1",
+      details: "Bring soda",
+      owner: "",
+      eventId: "11332252452453453"
+    },
+    {
+      id: "task2",
+      details: "Buy balloons",
+      owner: "",
+      eventId: "11332252452453453"
+    },
+    {
+      id: "task3",
+      details: "Buy disposable dishes and forks",
+      owner: "",
+      eventId: "11332252452453453"
+    },
+    {
+      id: "task1",
+      details: "Buy disposable glasses",
+      owner: "",
+      eventId: "11332252452453453"
+    }
+  ]
 
   return (
     <AcceptInviteContainer>
@@ -38,16 +79,45 @@ const AcceptInvite: React.FC = () => {
         title={eventName}
         subtitle={subtitle}
         imageUrl={EventImg}
-        eventText={eventText}
       />
       <Content>
+
+        {/* <TextBox heading1={heading1} heading2={heading2} /> */}
+        <div>
+          <h2>Event info</h2>
+          <EventCard eventText={eventText} />
+        </div>
+        
+        
+        <TasksContainer>
+          <h2>Tasks</h2>
+          <label>
+            <input type="radio" key={tasks[0].id} name="tasks" id={tasks[0].id} value={tasks[0].id}/>
+            {tasks[0].details}
+          </label>
+          <label>
+            <input type="radio" key={tasks[1].id} name="tasks" id={tasks[1].id} value={tasks[1].id}/>
+            {tasks[1].details}
+          </label>
+          <label>
+            <input type="radio" key={tasks[2].id} name="tasks" id={tasks[2].id} value={tasks[2].id}/>
+            {tasks[2].details}
+          </label>
+          <label>
+            <input type="radio" key={tasks[3].id} name="tasks" id={tasks[3].id} value={tasks[3].id}/>
+            {tasks[3].details}
+          </label>
+        </TasksContainer>
+      
+      </Content>
+      <div>
         <TextBox heading1={heading1} heading2={heading2} />
         <Btn
           primaryBtn={true}
           btnText="Register to the event"
           btnWidth="90%"
         />
-      </Content>
+      </div>
     </AcceptInviteContainer>
   )
 }
