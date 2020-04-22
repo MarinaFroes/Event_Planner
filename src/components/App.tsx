e4a786e9aeabab91043e4aa93b40129de72d9f29
MFAC <ninafroes@gmail.com>
2020-04-22 16:45:55 +0200

    feat: Add getUsers and hooks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import '../styles/App.css'
 
 import CreateEvent from './CreateEvent'
@@ -6,14 +6,27 @@ import EditEvent from './EditEvent'
 import AcceptInvite from './AcceptInvite'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import { getUsers } from '../utils/data'
+
+interface User {
+  id: string,
+  name: string,
+  email: string
+}
 
 const App: React.FC = () => {
+  const [users, setUsers] = useState<[User]>()
+  
+  useEffect(() => {
+    getUsers().then(res => setUsers(res))
+  }, [])
+
   return (
     <div className="App">
-      <NavBar isLogged={true} user="Marina"/>
-      {/* <CreateEvent />
-      <EditEvent /> */}
-      <AcceptInvite />
+      <NavBar isLogged={true} user={users?.[0].name}/>
+      <CreateEvent />
+      {/* <EditEvent />
+      <AcceptInvite /> */}
       <Footer />
     </div>
   );
