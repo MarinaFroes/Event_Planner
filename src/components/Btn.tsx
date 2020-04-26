45a2ce8a76af5b284c85f7b15d801242bf5479c9
MFAC <ninafroes@gmail.com>
2020-04-26 10:55:07 +0200

    feat: Add onClick prop to Btn component

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -17,15 +17,17 @@ interface Props {
   primaryBtn: boolean;
   btnText: string;
   btnWidth?: string;
-  btnType: "button" | "submit" | "reset" ;
+  btnType: "button" | "submit" | "reset";
+  onClick?: () => void;
 }
 
-const Btn: React.FC<Props> = ({ primaryBtn, btnText, btnWidth, btnType = "button"}) => {
+const Btn: React.FC<Props> = ({ primaryBtn, btnText, btnWidth, btnType = "button", onClick}) => {
   return (
     <StyledButton
       type={btnType}
       style={{ width: btnWidth }}
       primary={primaryBtn}
+      onClick={onClick}
     >
       {btnText}
     </StyledButton>
