0548b7196a46136e9c246c7eb2fbb06757b67eb6
MFAC <ninafroes@gmail.com>
2020-04-30 18:11:29 +0200

    feat: Provide store

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from '../src/redux/reducers/index'
+import middleware from '../src/redux/middleware'
+
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
