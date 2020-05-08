28a51a27a55b8185004d1ed04b5dcba9e2fbfa42
MFAC <ninafroes@gmail.com>
2020-05-08 16:02:10 +0200

    feat: Connect ViewUser to store showing message to user

--- src/components/ViewUser.tsx
+++ src/components/ViewUser.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { UserState } from '../store/users/types'
+import { AppState } from '../store/types'
+import { EventState } from '../store/events/types'
 
 
 const ViewUser: React.FC = () => {
+  let userName: string = ''
+  
+  const user: UserState = useSelector((state: AppState) => state.user)
+  const eventState: EventState = useSelector((state: AppState) => state.event)
+  console.log('eventState', eventState)
+
+  if (user.isLoggedIn) {
+    userName = user.user.name
+  }
+
   return (
       <div>
-        <p>User</p>
+        <p>{userName}, here are your events</p>
       </div>
     );
 }
