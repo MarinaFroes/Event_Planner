e4501aa4b05ed14a20a2d60b5389d503013a4232
MFAC <ninafroes@gmail.com>
2020-05-12 19:33:26 +0200

    feat: Refactor date services and include one to check if the event date is before today

--- src/services/formServices.ts
+++ src/services/formServices.ts
@@ -2,24 +2,52 @@ import { getLocalStorage } from '../utils/authDataRepository'
 
 import { FormData } from './formServicesTypes'
 
-export const getTodayDate = () => {
+const getTodayDate = (): string => {
   const today = new Date()
   const day = today.getDate()
   const month = today.getMonth() + 1
   const year = today.getFullYear()
+  return `${year}-${month}-${day}`
+}
+
+export const todayDateForInput = (): string => {
+  const date: string = getTodayDate()
+
+  const dateArray = date.split("-")
+  const [ yearString, monthString, dayString ] = dateArray
+  const month = Number(monthString)
+  const day = Number(dayString)
 
   let dd, mm
   (day < 10) ? (dd = '0' + day) : (dd = day);
   (month < 10) ? (mm = '0' + month) : (mm = month);
 
-  return year + '-' + mm + '-' + dd
+  return yearString + '-' + mm + '-' + dd
 }
 
-const formatDate = (date: string, time: string) => {
-  // date: "2020-12-03"
+const formatDate = (date: string) => {
+  //dateInput = 30-04-2020
+  const formattedDate = `${date.split("-").reverse().join("-")}`
+  // formattedDate = 2020-04-30
+  return formattedDate
+}
+
+export const isBeforeToday = (date: string) => {
+  const eventDate: any = new Date(formatDate(date))
+  const todayDate: any = new Date(getTodayDate())
+
+  if ( todayDate - eventDate < 0) {
+    return false
+  } else {
+    return true
+  }
+}
+
+const formatServerDate = (date: string, time: string) => {
+  // date: "2020-04-30"
   // time: "18:30"
   const formattedDate = `${date.split("-").reverse().join("-")} ${time}:00`
-  // formattedDate: "30-04-2020 12:12:00"
+  // formattedDate: "30-04-2020 18:30:00"
   return formattedDate
 }
 
@@ -29,7 +57,7 @@ export const formatEvent = (
   hostEmail: string
 ) => {
 
-  const date = formatDate(formData.date, formData.time)
+  const date = formatServerDate(formData.date, formData.time)
   
   const { title, additionalInfo, address, maxNumberGuest, totalCost, tasks } = formData
 
