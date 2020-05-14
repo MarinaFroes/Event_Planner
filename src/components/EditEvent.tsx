a68dc74497777b3ab206156c790432af4b24ec0f
MFAC <ninafroes@gmail.com>
2020-05-14 17:31:40 +0200

    feat: Show invite link when event is clicked and add alert to cancel event button

--- src/components/EditEvent.tsx
+++ src/components/EditEvent.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import Btn from './core/Btn'
 import Header from './core/Header'
 import EventForm from './EventForm'
-import EventImg from '../assets/images/tuva-mathilde-loland-4rfVL3NNGrA-unsplash.jpg'
+import EventImg from '../assets/images/edgar-castrejon-bG5rhvRH0JM-unsplash.jpg'
 import ShowGuests from './ShowGuests'
 import { editEvent } from '../utils/text'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppState, EventData } from '../store/types'
+import { handleSelectEvent } from '../store/events/eventActions'
+import { RouteComponentProps, Link } from 'react-router-dom'
 
 const EditEventContainer = styled.div`
   display: flex;
@@ -16,17 +20,33 @@ const EditEventContainer = styled.div`
   width: 100%;
 `
 
-const AlertLink = styled.a`
-  text-decoration: none;
+const AlertButton = styled.button`
+  background-color: var(--main-color-white, #fff);
+  border: none;
   color: var(--main-color-red, #bd0b2b);
   margin-bottom: 20px;
-
-  :hover {
-     cursor: pointer;
-  }
 `
 
-const EditEvent: React.FC = () => {
+type TParams = {
+  eid: string
+}
+
+const EditEvent: React.FC<RouteComponentProps<TParams>> = ({ match }) => {
+
+  const [showInviteLink, setShowInviteLink] = useState(false)
+
+  let isLoggedIn: boolean = useSelector((state: AppState) => state.user.isLoggedIn)
+
+  let selectedEvent: EventData | null = useSelector((state: AppState) => state.event.selectedEvent)
+  
+  const dispatch = useDispatch()
+  let eventId = match.params.eid //get id from param
+  
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(handleSelectEvent(eventId))
+    }
+  }, [dispatch, isLoggedIn, eventId])
 
   const guests = [
     {
@@ -73,26 +93,46 @@ const EditEvent: React.FC = () => {
     },
   ]
 
-  return (
-    <EditEventContainer>
-      <Header title="My Birthday Details" subtitle={editEvent.subtitle} imageUrl={EventImg}/>
-     
-      <EventForm
-        showImage={false}
-        heading1={editEvent.formHeading1}
-        heading2={editEvent.formHeading2}
-        primaryBtn={false}
-        btnText="Save updates"
-      />
+  if (selectedEvent !== null) {
+    
+    return (
+      <EditEventContainer>
+        <Header
+          title={`${selectedEvent.title[0].toUpperCase() + selectedEvent.title.slice(1)} Details`}
+          subtitle={editEvent.subtitle}
+          imageUrl={EventImg}
+        />
 
-      <Btn primaryBtn={true} btnText="Get invite link" btnType="submit"/>
-      
-      <AlertLink>
-        Cancel event
-      </AlertLink>
-      <ShowGuests guests={guests}/>
-    </EditEventContainer>
-  )
+        <EventForm
+          heading1={editEvent.formHeading1}
+          heading2={editEvent.formHeading2}
+          primaryBtn={false}
+          btnText="Save updates"
+        />
+
+        <Btn
+          primaryBtn={true}
+          btnText={showInviteLink ? "Hide invite link" : "Get invite link"}
+          btnType="submit"
+          onClick={() => setShowInviteLink(!showInviteLink)}
+        />
+        {
+          showInviteLink && (
+            <>
+              <p>Your friends can subscribe to the event using the link below:</p>
+              <Link to={`/invite/${eventId}`}>{`localhost:3000/invite/${eventId}`}</Link>
+            </>
+          )
+        }
+        <AlertButton onClick={() => alert('Are you sure you want to cancel this event?')}>
+          Cancel event
+        </AlertButton>
+        <ShowGuests guests={guests} />
+      </EditEventContainer>
+    )
+  }
+  return <p>No selected event</p>
+  
 }
 
 export default EditEvent
